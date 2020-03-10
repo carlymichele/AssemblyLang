bbf6e333e6c8b525efd829799609f28f10eed271
carlymichele <carlygordon97@gmail.com>
2020-03-10 16:25:53 -0700

    Refactored multbin and multhex functions (removed variable mult and added loop with ternary while condition for add functions)

--- Week4/REDO_Carly_Gordon_260_assign4.cpp
+++ Week4/REDO_Carly_Gordon_260_assign4.cpp
@@ -50,11 +50,11 @@ int main()
 
 	cout<<"Sample Run 4" << endl;
 	cout<<"binary 111111111110 * 10 = "<<multbin("111111111110", "10")<<endl;   //you should get 1111111111100
-	cout<<"hexadecimal 1CA0 * D4482 = "<<multhex("1CA0", "D4482")<<endl<<endl;  //you should get B0076EC900
+	cout<<"hexadecimal 1CA0 * D4482 = "<<multhex("1CA0", "D4482")<<endl<<endl;  //you should get 17BC909400
 
 	cout<<"Sample Run 5" << endl;
 	cout<<"binary 1 * 1 = "<<multbin("1", "1")<<endl;   //you should get 1
-	cout<<"hexadecimal 4B * 5D = "<<multhex("4B", "5D")<<endl<<endl;  //you should get 21C9
+	cout<<"hexadecimal 4B * 5D = "<<multhex("4B", "5D")<<endl<<endl;  //you should get 1B3F
 
 	system("PAUSE");
 	return 0;
@@ -63,37 +63,30 @@ int main()
 string multbin(string bin1, string bin2)
 {
 	string binProd = ""; //1. This variable stores the binary product to be calculated.
-	int mult = 0;
 
 	if(bin1 == "0" || bin2 == "0"){ //2. Base Case: if either number is 0, the product is 0.
 		binProd.append("0");
 	}else if(bin1 == "1" || bin2 == "2"){
-		(bin1 == "1" ? binProd.append(bin) : binprod.append(bin2));
+		(bin1 == "1" ? binProd.append(bin1) : binProd.append(bin2));
 	}else if (bin1.size() > bin2.size()){ //4. If bin1 is larger, use bin2 as multiplier
 		for(int bit = 0; bit < bin2.size(); bit++){
-			mult += (bin2.at(bit) == '1' ? exp2(bin2.size() - bit - 1) : 0);
-		}
-
-		for(int i = 0; i < mult; i++){
-			binProd = addbin(bin1, binProd);
+			for(int i = 0; i < (bin2.at(bit) == '1' ? exp2(bin2.size() - bit - 1) : 0); i++){
+				binProd = addbin(bin1, binProd);
+			}
 		}
 	}else{  /*5. If bin2 is larger than bin1, use bin1 as multiplier*/
-		for (int bit = 0; bit < bin1.size(); bit++){
-			mult += (bin1.at(bit) == '1' ? exp2(bin1.size() - bit - 1) : 0);
-		}
-
-		for(int i = 0; i < mult; i++){
-			binProd = addbin(bin2, binProd);
+		for(int bit = 0; bit < bin1.size(); bit++){
+			for(int i = 0; i < (bin1.at(bit) == '1' ? exp2(bin1.size() - bit - 1) : 0); i++){
+				binProd = addbin(bin2, binProd);
+			}
 		}
 	}
-
 	return binProd;
 }
 
 string multhex(string hex1, string hex2)
 {
 	string hexProd = ""; //6. This variable stores the hexadecimal product to be calculated.
-	int mult = 0;
 
 	if(hex1 == "0" || hex2 == "0"){
 		hexProd.append("0");
@@ -102,30 +95,24 @@ string multhex(string hex1, string hex2)
 	}else if(hex2 == "1"){
 		hexProd.append(hex1);
 	}else if(hex1.size() > hex2.size()){ //7. If hex1 is larger, use hex2 as multiplier
-		// 8. mult is incremented for each bit value by the digit/letter.
 		for(int bit = 0; bit < hex2.size(); bit++){
 			//9. Inside the for loop is a ternary operator that checks if each bit is a digit or a letter depending on the ASCII code value
 			// Value returned from ternary
 			for(int i = 0; i < (hex2.at(bit) <= 57 ? hex2.at(bit) - 48/*if digit*/ : hex2.at(bit) - 55/*if letter*/); i++){
-				mult += (pow(16, hex2.size() - bit - 1));
+				for(int i = 0; i < (pow(16, hex2.size() - bit - 1)); i++){
+					hexProd = addhex(hex1, hexProd);
+				}
 			}
-		}
-
-		for(int i = 0; i < mult; i++){ //10. Using the "mult" calculated above, the hexProd is added to itself "mult" times.
-			hexProd = addhex(hex1, hexProd);
 		}
 	}else{ /*11. If hex2 is larger than hex1, use hex1 as multiplier*/
 		for(int bit = 0; bit < hex1.size(); bit++){
 			for(int i = 0; i < (hex1.at(bit) <= 57 ? hex1.at(bit) - 48 : hex1.at(bit) - 55); i++){
-				mult += (pow(16, hex1.size() - bit - 1));
+				for(int i = 0; i < (pow(16, hex2.size() - bit - 1)); i++){
+					hexProd = addhex(hex2, hexProd);
+				}
 			}
 		}
-
-		for(int i = 0; i < mult; i++){
-			hexProd = addhex(hex2, hexProd);
-		}
 	}
-
 	return hexProd;
 }
 
